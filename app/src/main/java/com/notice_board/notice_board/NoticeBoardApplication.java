a6002677e9e07506a0abb335406854c35688b8ef
parkjuyeong0312 <dcmo77777@gmail.com>
2025-07-23 16:52:09 +0900

    Feat : User, Post, Like, Comment Entity Create

--- app/src/main/java/com/notice_board/notice_board/NoticeBoardApplication.java
+++ app/src/main/java/com/notice_board/notice_board/NoticeBoardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NoticeBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NoticeBoardApplication.class, args);
     }
